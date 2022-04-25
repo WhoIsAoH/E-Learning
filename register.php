d863210c70aea2d471b4a57a080c410c032d24b8
AP Hacker <aphacker@bhaJjaka>
2022-04-26 00:30:27 +0545

    register fixed

--- register.php
+++ register.php
@@ -93,7 +93,7 @@ if(isset($_POST["submit"]))
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                             </div>
-                            r
+                            
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
                                         <input type="text" class="form-control form-control-user"
